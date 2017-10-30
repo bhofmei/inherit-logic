6845fb918ed2ee441e5723c22a78cbe9844f30ff
Brigitte Hofmeister <bhofmei@gmail.com>
2017-10-30 14:52:40 -0400

    moved fridge service logic

--- public/app/scenario/fridge/fridge.component.ts
+++ public/app/scenario/fridge/fridge.component.ts
@@ -1,7 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { ScenarioService } from '../scenario.service';
+import { AuthenticationService } from '../../authentication/authentication.service'
 
 @Component({
     selector: 'fridge',
     templateUrl: './app/scenario/fridge/fridge.template.html'
 })
-export class FridgeComponent {}
+export class FridgeComponent {
+
+ user: any;
+  scenario: any;
+  fridge: any;
+  routingObserver: any;
+  errorMessage: string;
+
+  constructor(private _router: Router,
+               private _route: ActivatedRoute,
+               private _authenticationService: AuthenticationService,
+               private _scenarioService: ScenarioService) {
+  }
+
+  ngOnInit(){
+     this.user = this._authenticationService.user;
+
+    this.routingObserver = this._route.params.subscribe(params => {
+            let scenId = params['scenId'];
+      let userId = this.user.id;
+        console.log(this.user, scenId);
+            this._scenarioService
+                .getFridge(userId, scenId)
+                .subscribe(
+                fridge => {
+                  console.log(fridge);
+                    this.fridge = fridge;
+                },
+                error => this._router.navigate(['/'])
+                );
+        });
+  }
+
+  ngOnDestory(){
+    this.routingObserver.unsubscribe()
+  }
+}
--- public/app/scenario/scenario.component.ts
+++ public/app/scenario/scenario.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
-import { ScenarioService } from './scenario.service';
-import { AuthenticationService } from '../authentication/authentication.service'
 
 @Component({
   selector: 'scenario',
@@ -10,38 +7,4 @@ import { AuthenticationService } from '../authentication/authentication.service'
 })
 
 export class ScenarioComponent {
-  user: any;
-  scenario: any;
-  fridge: any;
-  routingObserver: any;
-  errorMessage: string;
-
-  constructor(private _router: Router,
-               private _route: ActivatedRoute,
-               private _authenticationService: AuthenticationService,
-               private _scenarioService: ScenarioService) {
-  }
-
-  ngOnInit(){
-     this.user = this._authenticationService.user;
-    console.log(this.user);
-    this.routingObserver = this._route.params.subscribe(params => {
-            let scenId = params['scenId'];
-      let userId = this.user.id;
-
-            this._scenarioService
-                .getFridge(userId, scenId)
-                .subscribe(
-                fridge => {
-                    this.fridge = fridge;
-                },
-                error => this._router.navigate(['/'])
-                );
-        });
-  }
-
-  ngOnDestory(){
-    this.routingObserver.unsubscribe()
-  }
-
 }
