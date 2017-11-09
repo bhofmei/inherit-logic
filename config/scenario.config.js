76e3d11b0b0626fb46d36e91599e509ab791155d
Brigitte Hofmeister <bhofmei@gmail.com>
2017-11-09 09:25:25 -0500

    additional scenario configs and data

--- config/scenario.config.js
+++ config/scenario.config.js
@@ -1,7 +1,13 @@
 /* scenario default data */
 module.exports = {
+  geneLength: 350,
   deleteSizes: [50, 50, 50, 95, 95, 95, 140, 155],
   deleteSpots: [-15, 10, 35, 60, 85, 110, 135, 160, 185, 210],
+  maxDeleteTries: 5000,
+  maxFrameTries: 300,
+  maxMuteTries: 200,
+  shiftOkDist: 25,
+  muteOkDist: 12,
   recFrame: 1,
   wildtypePhage: '{"numToMake": 1, "isWildType": true, "deletions": false, "comment": "Wild type phage"}',
   frameShiftPhage: '{"numToMake": 1, "isWildType": false, "frameshifts" : {"howMany" : [1,1], "mixed": "never", "readable": "any", "frameChoice": 0}, "deletions": false, "comment": "Mutant phage containing a single frame shift mutation"}',
