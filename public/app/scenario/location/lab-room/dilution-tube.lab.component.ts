886ecc269253b20673b9b27aca2cc0180778fade
Brigitte Hofmeister <bhofmei@gmail.com>
2017-12-01 13:53:09 -0500

    dilution tubes w/ serial dilution

--- public/app/scenario/location/lab-room/dilution-tube.lab.component.ts
+++ public/app/scenario/location/lab-room/dilution-tube.lab.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ScenarioGlobals } from '../../scenario.globals';
 //import { ExperimentService } from '../experiment.service'
 
 @Component({
@@ -9,38 +10,75 @@ import { FormsModule } from '@angular/forms';
 })
 export class DilutionTubeLabComponent {
 
-  private dilutionValue: number = 10;
-  private contents: Object;
-  private errorMessage: string = '';
+  private dilutionValue: number = ScenarioGlobals.defaultDilution;
+  private contents: Object[];
+  private visibleLabel: boolean[];
 
-  constructor(){ }
+  constructor(){
+    this.contents = [null, null, null, null];
+    this.visibleLabel = [true, false, false, false];
+  }
 
   clearTubes() {
     // reset all variables
-    this.dilutionValue = 10;
-    this.errorMessage = '';
+    this.dilutionValue = ScenarioGlobals.defaultDilution;
+    this.contents = [null, null, null, null];
+    this.visibleLabel = [true, false, false, false];
   }
 
-  canDrag(): boolean {
-    return (this.contents !== null && this.contents !== undefined);
+  canDrag(src: number): boolean {
+    return (this.contents[src] !== null);
   }
 
-  getClasses(): Object {
+  getClassesTube(src: number): Object {
     return {'dil-tube col-2': true,
-            'filled': (this.contents !== null && this.contents !== undefined)
+            'filled': (this.contents[src] !== null)
            }
   }
 
-  getData(src: string): Object {
-    // return data needed for plate
-    // take into account dilution values
-    return this.contents;
+  getClassesVal(src: number): Object {
+    return {
+      'dil-value': true,
+      'invisible': !this.visibleLabel[src]
+    }
   }
 
-  dropContents($event: any, src: string){
-    var incomingDat = $event.dragData;
+  canDrop(dest: number): any {
+  return (dragData: any) => {
+    if(dragData === null || dragData === undefined)
+      return false;
+    if(dragData.hasOwnProperty('src') === false){
+      return false
+    }
+    let src = dragData.src;
+    if(dest === 0 && (src === 'B' || src === 'K')){
+      return true;
+    } else if (dest > 0 && src === dest-1){
+      return true;
+    }
+    return false;
+  };
+}
+
+  getData(src: number): Object {
+    if(this.contents[src] !== null)
+      this.contents[src]['src'] = src;
+    return this.contents[src];
+  }
+
+  dropContents($event: any, dest: number){
+    var incomingDat = JSON.parse(JSON.stringify($event.dragData));
+    // if dest == 0, src must be B or K
+    // if dest > 0, src must be dest - 1
     if(incomingDat.hasOwnProperty('lawnType') && incomingDat.hasOwnProperty('phage')){
-      this.contents = incomingDat;
+      // dilute
+      for(let i = 0; i < incomingDat.phage.length; i++){
+        incomingDat.phage[i].numPhage /= this.dilutionValue;
+      }
+      this.contents[dest] = incomingDat;
+      if(dest < 3){
+        this.visibleLabel[dest+1] = true;
+      }
     }
   }
 }
