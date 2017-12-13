2eab27cff7db1837422a1a7758e766033aca6808
Brigitte Hofmeister <bhofmei@gmail.com>
2017-12-13 10:13:41 -0500

    simplify landing room observables

--- public/app/scenario/location/landing-room/landing-room.component.ts
+++ public/app/scenario/location/landing-room/landing-room.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 
 import { ScenarioService } from '../../scenario.service';
+import { Scenario } from '../../../interfaces/scenario.interface';
 
 @Component({
   selector: 'landing-room',
@@ -11,7 +12,7 @@ import { ScenarioService } from '../../scenario.service';
 export class LandingRoomComponent {
 
   scenario: any;
-  routingObserver: any;
+  private subscription: any;
 
   constructor(private _router: Router,
                private _route: ActivatedRoute,
@@ -20,21 +21,18 @@ export class LandingRoomComponent {
   }
 
   ngOnInit(){
-    this.routingObserver = this._route.parent.params.subscribe(params => {
-            let scenId = params['scenId'];
-            this._scenarioService
-                .getScenario(scenId)
-                .subscribe(
-                scenario => {
-                    this.scenario = scenario;
-                },
-                error => this._router.navigate(['/'])
-                );
-        });
+    let scenId = this._route.parent.snapshot.paramMap.get('scenId');
+    this.subscription = this._scenarioService
+      .getScenario(scenId)
+      .subscribe(
+      scenario => {
+        this.scenario = scenario;
+      },
+      error => this._router.navigate(['/'])
+    );
   }
 
   ngOnDestory(){
-    this.routingObserver.unsubscribe()
+    this.subscription.unsubscribe();
   }
-
 }
