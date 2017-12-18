41431bb6d3476910070e19b3b28434e33b15800b
Brigitte Hofmeister <bhofmei@gmail.com>
2017-12-18 10:15:17 -0500

    clean up course model

--- app/controllers/index.server.controller.js
+++ app/controllers/index.server.controller.js
@@ -1,14 +1,7 @@
 // Create a new 'render' controller method
 exports.render = function(req, res) {
-	// Set the safe user object
-	const user = (!req.user) ? null : {
-		id: req.user.userId,
-		name: req.user.name
-	};
-
 	// Use the 'response' object to render the 'index' view with a 'title' and 'user' properties
 	res.render('index', {
-		title: 'Cricket',
-		user: JSON.stringify(user)
+		title: 'Cricket'
 	});
 };
--- app/models/course.server.model.js
+++ app/models/course.server.model.js
@@ -6,16 +6,17 @@ const CourseSchema = new Schema({
     type: String,
     trim: true,
     required: 'Course num cannot be blank',
-    unique: 'Course num must be unique'
+    //unique: 'Course num must be unique'
   },
-  instructor: {
+  description: String,
+  instructors: [{
     type: Schema.ObjectId,
     ref: 'User'
-  },
+  }]/*,
   scenarios: [{
     type: Schema.ObjectId,
     ref: "Scenario"
-  }]
+  }]*/
 });
 
 CourseSchema.statics.findOneByNum = function(cNum, callback){
