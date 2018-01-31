666785fc5782c2a7d56b6c30bf5363436041ff1f
Brigitte Hofmeister <bhofmei@gmail.com>
2018-01-31 15:17:30 -0500

    clean up course model and tests

--- app/models/course.server.model.js
+++ app/models/course.server.model.js
@@ -13,30 +13,8 @@ const CourseSchema = new Schema({
   instructors: [{
     type: Schema.ObjectId,
     ref: 'User'
-  }]/*,
-  scenarios: [{
-    type: Schema.ObjectId,
-    ref: "Scenario"
-  }]*/
+  }]
 });
-
-CourseSchema.statics.findOneByNum = function(cNum, callback){
-  this.findOne({courseNum: cNum}, callback);
-};
-
-CourseSchema.statics.courseExists = function(cNum){
-  if(cNum === null){
-    return 0;
-  }
-  this.findOne({courseNum: cNum}, (err, res)=>{
-    if(err)
-      return 0;
-    else if(!res)
-      return -1;
-    return 1;
-  });
-
-};
 
 CourseSchema.set('toJSON',{getters: true});
 
