00d838684d356910cfddf5bf80cd11791a517222
Brigitte Hofmeister <bhofmei@gmail.com>
2018-01-31 15:05:34 -0500

    prod mode angular

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,9 +1,15 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
+const webpack = require('webpack');
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = merge(common, {
   plugins: [
-    new UglifyJSPlugin()
+    new UglifyJSPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.ENV': JSON.stringify('production')
+
+    })
   ]
 });
