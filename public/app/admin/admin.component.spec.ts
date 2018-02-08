9d5bc625f1e5c26d5fe553ad2290b1f54045de08
Brigitte Hofmeister <bhofmei@gmail.com>
2018-02-08 09:40:17 -0500

    admin comp testing and docs

---
+++ public/app/admin/admin.component.spec.ts
@@ -0,0 +1,99 @@
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { McBreadcrumbsModule } from 'ngx-breadcrumbs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { DebugElement } from '@angular/core';
+import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
+import { RouterLinkStubDirective, getAllRouterLinks } from '../testing';
+
+import { AdminComponent } from './admin.component';
+import { AuthenticationService } from '../authentication/authentication.service';
+
+import { User } from '../interfaces/user.interface';
+
+const testAdmin: User = {
+    id: 1,
+    firstName: 'Admin',
+    lastName: 'Last',
+    email: 'test@test.com',
+    role: 2 // admin
+}
+
+const testInstr: User = {
+  id: 2,
+  firstName: 'Instr',
+  lastName: 'Last',
+  email: 'instr@test.com',
+  role: 1 // instr
+}
+
+class AuthServiceStub {
+  getUser(): User {
+    return testAdmin;
+  }
+}
+
+describe('Admin Component', ()=>{
+  let fixture: ComponentFixture<AdminComponent>;
+  let authService: AuthenticationService;
+
+
+  beforeEach(async()=>{
+    TestBed.configureTestingModule({
+      imports: [McBreadcrumbsModule.forRoot(), RouterTestingModule],
+      declarations: [AdminComponent, RouterLinkStubDirective],
+      providers: [
+        {provide: AuthenticationService, useClass: AuthServiceStub}
+      ]
+    }).compileComponents();
+  }) // end beforeEach async
+
+  beforeEach(()=>{
+    fixture = TestBed.createComponent(AdminComponent);
+    authService = TestBed.get(AuthenticationService);
+  }); // end beforeEach
+
+  describe('Check routes', ()=>{
+    let links;
+    beforeEach(()=>{
+      fixture.detectChanges();
+      links = getAllRouterLinks(fixture);
+    }); // end beforeEach
+
+    it('Should have courses link', ()=>{
+      let linkEl = links[0];
+      expect(linkEl.linkParams[0]).toMatch(/courses/);
+    }); // end Should have courses link
+
+    it('Should have students link', ()=>{
+      let linkEl = links[1];
+      expect(linkEl.linkParams[0]).toMatch(/students/);
+    }); // end Should have students link
+  }); // end Check routes
+
+  describe('With admin', ()=>{
+    it('Should have admin header', ()=>{
+      fixture.detectChanges();
+      let h = fixture.debugElement.query(By.css('h2'));
+      let hText = h.nativeElement.innerHTML;
+      expect(hText).toBe('Administrator Page');
+    }); // end Should have admin header
+  }); // end With admin
+
+  describe('With instr', ()=>{
+    let links;
+    beforeEach(()=>{
+      authService.getUser = ()=>{return testInstr};
+      fixture.detectChanges();
+      links = getAllRouterLinks(fixture);
+    }); // end beforeEach
+
+    it('Should have instr header', ()=>{
+      fixture.detectChanges();
+      let h = fixture.debugElement.query(By.css('h2'));
+      let hText = h.nativeElement.innerHTML;
+      expect(hText).toBe('Instructor Page');
+    }); // end Should have instr header
+  }); // end With instr
+
+}); // end Admin Component
--- public/app/admin/admin.component.ts
+++ public/app/admin/admin.component.ts
@@ -5,15 +5,23 @@ import 'rxjs/add/operator/takeUntil'
 import { AuthenticationService } from '../authentication/authentication.service';
 import { User } from '../interfaces/user.interface';
 
+/**
+ * Entrance component for admin features
+ * Mainly, it is a card that allows navigation between course-related
+ * info and student-related info
+ * Content of the card is determined by router
+ */
 @Component({
   selector: 'admin',
   templateUrl: 'app/admin/admin.template.html'
 })
 
 export class AdminComponent{
+  /**
+   * Logged in user
+   */
   private adminUser: User;
   private errorMessage: string = '';
-  //private isDestroyed$: Subject<boolean>
 
   constructor(
     private _authenticationService: AuthenticationService
