6c17ff4e6041eec57b9c0289233925a5fc7e5523
Brigitte Hofmeister <bhofmei@gmail.com>
2018-02-15 09:21:30 -0500

    fixed instr/student types

--- public/app/interfaces/course.interface.ts
+++ public/app/interfaces/course.interface.ts
@@ -4,6 +4,6 @@ import { _User } from './user.interface';
 export interface Course {
   courseNum: string;
   description: string;
-  students?: [Student];
-  instructors?: [_User]
+  students?: Student[];
+  instructors?: _User[]
 }
