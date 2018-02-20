51e033e5030db3468a2044cadf7fab27fa89d911
Brigitte Hofmeister <bhofmei@gmail.com>
2018-02-20 10:32:15 -0500

    remove limiter

--- app/routes/user.server.routes.js
+++ app/routes/user.server.routes.js
@@ -1,24 +1,33 @@
 const user = require('../controllers/user.server.controller');
 const scenarios = require('../controllers/scenario.server.controller');
 const passport = require('passport');
-const limiter = require('../../config/config').limiter;
+//const limiter = require('../../config/config').limiter;
 
 module.exports = function(app){
-    app.route('/api/auth/signup').post(limiter.authLimiter.prevent, user.signup);
-    app.route('/api/auth/signin').post(limiter.authLimiter.prevent, user.signin);
+    //app.route('/api/auth/signup').post(limiter.authLimiter.prevent, user.signup);
+  app.route('/api/auth/signup').post(user.signup);
+//    app.route('/api/auth/signin').post(limiter.authLimiter.prevent, user.signin);
+  app.route('/api/auth/signin').post(user.signin);
     app.route('/api/auth/signout').get(user.signout);
 
     // functions to reset password
-  app.route('/api/auth/forget-password').post(limiter.authLimiter.prevent, user.resetPasswordEmail);
-  app.route('/api/auth/reset-password').post(limiter.authLimiter.prevent,user.resetPassword);
+  //app.route('/api/auth/forget-password').post(limiter.authLimiter.prevent, user.resetPasswordEmail);
+  app.route('/api/auth/forget-password').post( user.resetPasswordEmail);
+//  app.route('/api/auth/reset-password').post(limiter.authLimiter.prevent,user.resetPassword);
+  app.route('/api/auth/reset-password').post(user.resetPassword);
 
   // get/update information for specific user
-    app.route('/api/users/:userId')
+//    app.route('/api/users/:userId')
+//      .get(user.requiresLogin, user.getUser)
+//      .post(limiter.userUpdateLimiter.prevent, user.requiresLogin, user.editUser);
+  app.route('/api/users/:userId')
       .get(user.requiresLogin, user.getUser)
-      .post(limiter.userUpdateLimiter.prevent, user.requiresLogin, user.editUser);
+      .post(user.requiresLogin, user.editUser);
 
+//  app.route('/api/users/:userId/update-password')
+//    .post(limiter.userUpdateLimiter.prevent, user.requiresLogin, user.updatePassword);
   app.route('/api/users/:userId/update-password')
-    .post(limiter.userUpdateLimiter.prevent, user.requiresLogin, user.updatePassword);
+    .post(user.requiresLogin, user.updatePassword);
 
 
   app.param('userId', user.userById);
