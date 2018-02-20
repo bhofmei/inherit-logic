3c84a515723dc274367c87bb39f8bcc190318751
Brigitte Hofmeister <bhofmei@gmail.com>
2018-02-20 10:37:58 -0500

    remove genetics rate limiters

--- app/routes/genetics.server.routes.js
+++ app/routes/genetics.server.routes.js
@@ -1,12 +1,16 @@
 const genetics = require('../controllers/genetics.server.controller');
 //const limiter = require('../../config/rate-limit/rate-limit.' + process.env.NODE_ENV+'.js');
-const limiter = require('../../config/config').limiter;
+//const limiter = require('../../config/config').limiter;
 
 module.exports = function (app) {
 
+//  app.route('/api/cricket/plate')
+//    .post(limiter.geneticsLimiter.prevent, genetics.createPlate);
   app.route('/api/cricket/plate')
-    .post(limiter.geneticsLimiter.prevent, genetics.createPlate);
+    .post(genetics.createPlate);
 
   app.route('/api/cricket/plexer')
     .post(limiter.geneticsLimiter.prevent, genetics.handlePlexer);
+  app.route('/api/cricket/plexer')
+    .post(genetics.handlePlexer);
 };
