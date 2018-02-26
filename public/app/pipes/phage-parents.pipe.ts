0048cede8a66c402f3139f7489486c4f3f1e5133
Brigitte Hofmeister <bhofmei@gmail.com>
2018-02-26 15:43:19 -0500

    phage strain num error on view parents

--- public/app/pipes/phage-parents.pipe.ts
+++ public/app/pipes/phage-parents.pipe.ts
@@ -10,13 +10,15 @@ export class PhageParentsPipe implements PipeTransform {
       numParents = parents.length
     }
     let sorted: Phage[] = parents.sort((n1, n2)=>{return n1.strainNum - n2.strainNum});
+    let nums = sorted.map((phage)=>{return (phage.strainNum+1).toString()});
 
     if(sorted.length === 1 && numParents === 1){
-      out = 'Strain ' + sorted[0].strainNum.toString();
+
+      out = 'Strain ' + nums[0];
     } else if(sorted.length === 2 && numParents === 1){
-      out = 'Strains ' + sorted[0].strainNum.toString() + ' and ?';
+      out = 'Strains ' + nums[0] + ' and ?';
     } else if(sorted.length === 2 && numParents === 2){
-      out = 'Strains ' + sorted[0].strainNum.toString() + ' and ' + sorted[1].strainNum.toString();
+      out = 'Strains ' + nums[0] + ' and ' + nums[1];
     }
     return out;
   }
