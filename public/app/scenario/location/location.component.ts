7e58cc2222635354f6c3298cb4ae965b7e915ae1
Brigitte Hofmeister <bhofmei@gmail.com>
2018-04-30 09:32:40 -0400

    non-relative template path name

--- public/app/scenario/location/location.component.ts
+++ public/app/scenario/location/location.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'location',
-  templateUrl: './app/scenario/location/location.template.html',
-  styleUrls: ['./app/scenario/location/location.style.css']
+  templateUrl: 'app/scenario/location/location.template.html',
+  styleUrls: ['app/scenario/location/location.style.css']
 })
 
 export class LocationComponent {
