aab2e1ec747be4d2f6111a468f4343757dd2c580
Brigitte Hofmeister <bhofmei@gmail.com>
2018-08-30 12:27:01 -0400

    update signin comp tests

--- public/app/authentication/signin/signin.component.spec.ts
+++ public/app/authentication/signin/signin.component.spec.ts
@@ -19,8 +19,7 @@ let authService: AuthenticationService;
 let route: RouterStub
 
 describe('Signin Component', ()=>{
-
-  beforeEach(async(()=>{
+    beforeEach(async(()=>{
     route = new RouterStub();
     TestBed.configureTestingModule({
       imports: [SharedModule],
@@ -32,7 +31,7 @@ describe('Signin Component', ()=>{
     }).compileComponents();
   })); // end beforeEach async
 
-  describe('Test forget passsword link', ()=>{
+    describe('Test forget passsword link', ()=>{
     beforeEach(fakeAsync(()=>{
       createComponent();
       tick();
@@ -50,8 +49,119 @@ describe('Signin Component', ()=>{
 
   }); // end Test links
 
-  describe('Test error messages', ()=>{
-    let aService, signinSpy;
+  describe('Test form validity', ()=>{
+    beforeEach(fakeAsync(()=>{
+      createComponent();
+      tick();
+    })); // end beforeEach fakeAsync
+
+    it('should be invalid when empty', ()=>{
+      expect(comp.credentials.valid).toBeFalsy();
+    }); // end should be invalid when empty
+
+    it('should have invalid email', ()=>{
+      let email = comp.credentials.controls['username'];
+      email.setValue('invaidemail');
+      expect(email.valid).toBeFalsy();
+      let errors = email.errors || {};
+      expect(errors['email']).toBeTruthy();
+    }); // end should have invalid email
+
+    it('should have valid email', ()=>{
+      let email = comp.credentials.controls['username'];
+      email.setValue(userAdmin.email);
+      expect(email.valid).toBeTruthy();
+      let errors = email.errors || {};
+      expect(errors['email']).toBeFalsy();
+    }); // end should have valid email
+
+    it('should have too short of password', ()=>{
+      let password = comp.credentials.controls['password'];
+      password.setValue('abc');
+      expect(password.valid).toBeFalsy();
+      let errors = password.errors || {};
+      expect(errors['minlength']).toBeTruthy();
+    }); // end should ahve too short of password
+
+    it('should have acceptable password', ()=>{
+      let password = comp.credentials.controls['password'];
+      password.setValue('long-acceptable-password');
+      expect(password.valid).toBeTruthy();
+      let errors = password.errors || {};
+      expect(errors['minlength']).toBeFalsy();
+    }); // end should have acceptable password
+  }); // end Test form validity
+
+  describe('Test form warnings', ()=>{
+    beforeEach(fakeAsync(()=>{
+      createComponent();
+      tick();
+      page.addElements();
+    })); // end beforeEach fakeAsync
+
+    it('should have no warnings', ()=>{
+      expect(page.warnings.length).toBe(0);
+    }); // end should have no warnings
+
+    it('should have email required warning', fakeAsync(()=>{
+      let email = comp.credentials.controls['username'];
+      email.setValue('');
+      email.markAsTouched();
+      fixture.detectChanges();
+      page.addElements();
+      expect(page.warnings.length).toBe(1);
+      let warning = page.warnings[0].nativeElement.innerHTML;
+      expect(warning).toBe('Email is required.')
+    })); // end should have email required warning
+
+    it('should have email invalid warning', fakeAsync(()=>{
+      let email = comp.credentials.controls['username'];
+      email.setValue('invalid-email');
+      email.markAsTouched();
+      fixture.detectChanges();
+      page.addElements();
+      expect(page.warnings.length).toBe(1);
+      let warning = page.warnings[0].nativeElement.innerHTML;
+      expect(warning).toBe('Email is invalid.')
+    })); // end should have email invalid warning
+
+    it('should have password required warning', fakeAsync(()=>{
+      let password = comp.credentials.controls['password'];
+      password.setValue('');
+      password.markAsTouched();
+      fixture.detectChanges();
+      page.addElements();
+      expect(page.warnings.length).toBe(1);
+      let warning = page.warnings[0].nativeElement.innerHTML;
+      expect(warning).toBe('Password is required.')
+    })); // end should have password required warning
+
+    it('should have password length warning', fakeAsync(()=>{
+      let password = comp.credentials.controls['password'];
+      password.setValue('abc');
+      password.markAsTouched();
+      fixture.detectChanges();
+      page.addElements();
+      expect(page.warnings.length).toBe(1);
+      let warning = page.warnings[0].nativeElement.innerHTML;
+      expect(warning).toBe('Password must be at least 6 characters.')
+    })); // end should have password length warning
+
+    it('should have no warnings with valid input', fakeAsync(()=>{
+      let email = comp.credentials.controls['username'];
+      email.setValue('valid@email.com');
+      email.markAsTouched();
+      let password = comp.credentials.controls['password'];
+      password.setValue('abcdefghi');
+      password.markAsTouched();
+      fixture.detectChanges();
+      page.addElements();
+      expect(page.warnings.length).toBe(0);
+    })); // end should have no warnings with valid input
+  }); // end Test form warnings
+
+  describe('Test form submission', ()=>{
+        let aService, signinSpy;
     let testEmail = userAdmin.email;
     beforeEach(fakeAsync(()=>{
       createComponent();
@@ -62,140 +172,80 @@ describe('Signin Component', ()=>{
       addMatchers();
     })); // end beforeEach fakeAsync
 
-    it('Should update on "Submit"', ()=>fakeAsync(()=>{
-      click(page.submitButton);
+    it('should have disabled button', ()=>{
+     var isDisabled = page.submitButton.nativeElement.hasAttribute('disabled');
+      expect(isDisabled).toBeTruthy();
+    }); // end should have disabled button
+
+    it('should not have disabled button', fakeAsync(()=>{
+      let email = comp.credentials.controls['username'];
+      email.setValue(testEmail);
+      let password = comp.credentials.controls['password'];
+      password.setValue('abcdefgh');
+      fixture.detectChanges();
+      page.addElements();
+      var isDisabled = page.submitButton.nativeElement.hasAttribute('disabled');
+      expect(isDisabled).toBeFalsy();
+    })); // end should not have disabled button
+
+    it('should submit', fakeAsync(()=>{
+      let email = comp.credentials.controls['username'];
+      email.setValue(testEmail);
+      let password = comp.credentials.controls['password'];
+      password.setValue('abcdefgh');
+      fixture.detectChanges();
+//      page.addElements();
+//      click(page.submitButton);
+      comp.signin();
       let didUpdate = signinSpy.calls.any();
       expect(didUpdate).toBe(true);
-    })); // end Should spy on update when click save
+    })); // end should submit
 
-  it('Should have error message with no email and password', fakeAsync(()=>{
-      comp.signin();
-      tick();
-      fixture.detectChanges();
-      page.addElements();
-      expect(page.errorMessage).not.toBeNull();
-      let message = page.errorMessage.innerHTML;
-      expect(message).toTemplateMatch('Missing Credentials');
-    })); // end Should have error message with no email and password
 
-     it('Should have error message with no password', fakeAsync(()=>{
-      page.emailInput.value = testEmail;
-       page.emailInput.dispatchEvent(new Event('input'));
-       fixture.detectChanges();
-      comp.signin();
-      tick();
-      fixture.detectChanges();
-      page.addElements();
-      expect(page.errorMessage).not.toBeNull();
-      let message = page.errorMessage.innerHTML;
-      expect(message).toTemplateMatch('Missing Credentials');
-    })); // end Should have error message with no password
-
-    it('Should have error message with no email', fakeAsync(()=>{
-      page.passwordInput.value = 'test-password';
-      page.passwordInput.dispatchEvent(new Event('input'));
-      fixture.detectChanges();
-      comp.signin();
-      tick();
-      fixture.detectChanges();
-      page.addElements();
-      expect(page.errorMessage).not.toBeNull();
-      let message = page.errorMessage.innerHTML;
-      expect(message).toTemplateMatch('Missing Credentials');
-    })); // end Should have error message with no email
-
-     it('Should have error message with incorrect email', fakeAsync(()=>{
-      page.emailInput.value = 'fake@email.com';
-      page.emailInput.dispatchEvent(new Event('input'));
-      page.passwordInput.value = 'password';
-       page.passwordInput.dispatchEvent(new Event('input'));
-       fixture.detectChanges();
-        comp.signin();
-        tick(); fixture.detectChanges();
-      page.addElements();
-      expect(page.errorMessage).not.toBeNull();
-      let message = page.errorMessage.innerHTML;
-      expect(message).toTemplateMatch('User not found');
-    })); // end Should have error message with incorrect email
-
-      it('Should have error message with incorrect password', fakeAsync(()=>{
-        page.emailInput.value = testEmail;
-        page.emailInput.dispatchEvent(new Event('input'));
-      page.passwordInput.value = 'invalid';
-         page.passwordInput.dispatchEvent(new Event('input'));
+      it('should navigate home on submit', fakeAsync(()=>{
+        comp.credentials.controls['username'].setValue(testEmail);
+        comp.credentials.controls['password'].setValue('abcdefgh');
         fixture.detectChanges();
-      comp.signin();
-      tick();
-      fixture.detectChanges();
-      page.addElements();
-      expect(page.errorMessage).not.toBeNull();
-      let message = page.errorMessage.innerHTML;
-      expect(message).toTemplateMatch('Invalid password');
-    })); // end Should have error message with incorrect password
-  }); // end Test error messages
+        comp.signin();
+        tick();
+        fixture.detectChanges();
+        let pathNav = route.path;
+        expect(pathNav).toBe('/');
+      })); // end should navigate home on submit
 
-  describe('Test successful signin', ()=>{
-    let aService;
-    let testEmail = userAdmin.email;
-    beforeEach(fakeAsync(()=>{
-      createComponent();
-      tick();
-      page.addElements();
-      aService = fixture.debugElement.injector.get(AuthenticationService);
-      addMatchers();
-    })); // end beforeEach fakeAsync
-
-    it('should navigate home', fakeAsync(()=>{
-      page.emailInput.value = testEmail;
-      page.emailInput.dispatchEvent(new Event('input'));
-      page.passwordInput.value = 'password';
-      page.passwordInput.dispatchEvent(new Event('input'));
-      fixture.detectChanges();
-      comp.signin();
-      tick();
-      fixture.detectChanges();
-      let pathNav = route.path;
-      expect(pathNav).toBe('/');
-    })); // end should navigate home
-
-    it('should naviate redirect', fakeAsync(()=>{
+    it('should navigate redirect on submit', fakeAsync(()=>{
       let testRedirect = '/admin';
       aService.redirectUrl = testRedirect;
       tick();
-      page.emailInput.value = testEmail;
-      page.emailInput.dispatchEvent(new Event('input'));
-      page.passwordInput.value = 'password';
-      page.passwordInput.dispatchEvent(new Event('input'));
-      fixture.detectChanges();
-      comp.signin();
-      tick();
-      fixture.detectChanges();
-      let pathNav = route.path;
-      expect(pathNav).toBe(testRedirect);
-    }));
-  }); // end Test successful signin
+        comp.credentials.controls['username'].setValue(testEmail);
+        comp.credentials.controls['password'].setValue('abcdefgh');
+        fixture.detectChanges();
+        comp.signin();
+        tick();
+        fixture.detectChanges();
+        let pathNav = route.path;
+        expect(pathNav).toBe(testRedirect);
+      })); // end should navigate redirect on submit
+  }); // end Test form submission
 }); // end Signin Component
 
 class Page {
-  errorMessage: HTMLElement;
-  submitButton: DebugElement;
-  emailInput: HTMLInputElement;
-  passwordInput: HTMLInputElement;
   links: RouterLinkStubDirective[];
+  warnings: DebugElement[];
+  submitButton: DebugElement;
 
   constructor(){}
 
   addElements(){
-    // error message
-    this.errorMessage = fixture.debugElement.query(By.css('.text-danger')).nativeElement;
-    this.submitButton = fixture.debugElement.query(By.css('button'));
-
-    this.emailInput = fixture.debugElement.query(By.css('#inputEmail')).nativeElement;
-    this.passwordInput = fixture.debugElement.query(By.css('#inputPassword')).nativeElement;
     let linkDes = fixture.debugElement
     .queryAll(By.directive(RouterLinkStubDirective));
     this.links = linkDes
     .map(de => de.injector.get(RouterLinkStubDirective) as RouterLinkStubDirective);
+    let possibleWarnings = fixture.debugElement.queryAll(By.css('.invalid-feedback'));
+    this.warnings = possibleWarnings.map((w)=>{
+      return w.query(By.css('div'))
+    });
+    this.submitButton = fixture.debugElement.query(By.css('button'));
   }
 }
 
